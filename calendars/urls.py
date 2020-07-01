414316ae1b11c745829d626cbd2f6566a36be56e
Elise <elisemgielen@gmail.com>
2020-07-01 12:50:22 +0200

    Initial calendar shown

--- calendars/urls.py
+++ calendars/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='calendars-home'),
+    path('month/', views.monthly, name='calendars-month'),
+    path('year/', views.yearly, name='calendars-year'),
 ]
--- calendars/views.py
+++ calendars/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 import calendar
 import datetime
 import time
 
 def home(request):
-    cal = calendar.Calendar(0)
+    return redirect('calendars-month')
+
+def monthly(request):
+    cal = calendar.Calendar(0) #0 is the default!
     today = datetime.date.today()
     context = {
         'month': cal.itermonthdates(today.year, today.month)
     }
     return render(request, 'calendars/monthly.html', context)
+
+def yearly(request):
+    cal = calendar.Calendar(0)
+    today = datetime.date.today()
+
+    year = []
+    for x in range(1,13):
+        year.append(cal.itermonthdates(today.year, x))
+
+    context = {
+        'year': year
+    }
+    return render(request, 'calendars/yearly.html', context)
