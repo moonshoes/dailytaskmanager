99461e331a23504357668be3dbe4576a7a6c3fb7
Elise <elisemgielen@gmail.com>
2020-07-03 13:29:35 +0200

    Initial day overview html template

---
+++ calendars/templatetags/calendars_filters.py
@@ -0,0 +1,22 @@
+from django import template
+import operator
+
+register = template.Library()
+
+@register.filter(name='range')
+def forRange(start, end):
+    return range(start, end)
+
+@register.filter()
+def hour(value):
+    if value == 0 or value == 12:
+        remainder = 12
+    else:
+        remainder = operator.mod(value, 12)
+
+    if value < 12:
+        suffix = 'AM'
+    else:
+        suffix = 'PM'
+
+    return '{:d} {}'.format(remainder, suffix)
