d526e6558855bed5ec7388bc1da05b68012f2014
Elise <elisemgielen@gmail.com>
2020-07-03 14:06:44 +0200

    [Initial commit] Returns week without proper formatting

--- calendars/urls.py
+++ calendars/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('month/', views.monthly, name='calendars-month'),
     path('year/', views.yearly, name='calendars-year'),
     path('day/', views.daily, name='calendars-day'),
+    path('week/', views.weekly, name='calendars-week'),
 ]
