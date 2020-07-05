a806703bbfb452658ad7502254f2303ddb15d4d8
Elise <elisemgielen@gmail.com>
2020-07-05 13:28:48 +0200

    Previous and next overviews are linked

--- calendars/urls.py
+++ calendars/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name='calendars-home'),
-    path('month/', views.monthly, name='calendars-month'),
     path('year/', views.yearly, name='calendars-year'),
-    path('day/', views.daily, name='calendars-day'),
+    path('year/<int:yearArg>/', views.yearly, name='calendars-year'),
+    path('month/', views.monthly, name='calendars-month'),
+    path('month/<int:yearArg>/<int:monthArg>/', views.monthly, name='calendars-month'),
     path('week/', views.weekly, name='calendars-week'),
+    path('week/<int:yearArg>/<int:monthArg>/<int:dayArg>/', views.weekly, name='calendars-week'),
+    path('day/', views.daily, name='calendars-day'),
+    path('day/<int:yearArg>/<int:monthArg>/<int:dayArg>/', views.daily, name='calendars-day'),
 ]
--- calendars/views.py
+++ calendars/views.py
@@ -6,49 +6,120 @@ import time
 def home(request):
     return redirect('calendars-month')
 
-def monthly(request):
+def monthly(request, yearArg=-1, monthArg=-1):
     cal = calendar.Calendar(0) #0 is the default!
-    today = datetime.date.today()
+    if yearArg and monthArg == -1:
+        today = datetime.date.today()
+        year = today.year
+        month = today.month
+    else:
+        year = yearArg
+        month = monthArg
+    
+    if month > 1 and month < 12:
+        prevMonth = month - 1
+        nextMonth = month + 1
+    elif month == 1:
+        prevMonth = 12
+        nextMonth = month + 1
+        year = year - 1
+    else:
+        prevMonth = month + 1
+        nextMonth = 1
+        year = year + 1
+
     context = {
-        'month': cal.itermonthdates(today.year, today.month)
+        'prevMonth': prevMonth,
+        'month': calendar.month_name[month],
+        'nextMonth': nextMonth,
+        'year': year,
+        'monthList': cal.itermonthdates(year, month)
     }
     return render(request, 'calendars/monthly.html', context)
 
-def yearly(request):
+def yearly(request, yearArg=-1):
     cal = calendar.Calendar(0)
-    today = datetime.date.today()
+    if yearArg == -1:
+        today = datetime.date.today()
+        year = today.year
+    else:
+        year = yearArg
 
-    year = []
+    yearList = []
     for x in range(1,13):
         monthinfo = {
             'monthname': calendar.month_name[x],
-            'month': cal.itermonthdates(today.year, x)
+            'month': cal.itermonthdates(year, x)
         }
-        year.append(monthinfo)
+        yearList.append(monthinfo)
 
     context = {
-        'year': year
+        'prevYear': year - 1,
+        'year': year,
+        'nextYear': year + 1,
+        'yearList': yearList
     }
     return render(request, 'calendars/yearly.html', context)
 
-def daily(request):
-    today = datetime.date.today()
+def daily(request, yearArg=-1, monthArg=-1, dayArg=-1):
+    if yearArg == -1 and monthArg == -1 and dayArg == -1:
+        today = datetime.date.today()
+    else:
+        today = datetime.date(yearArg, monthArg, dayArg)
+    
+    delta = datetime.timedelta(days=1)
+    prevDay = today - delta
+    nextDay = today + delta
+
     context = {
+        'prevDay': {
+                'year': prevDay.year,
+                'month': prevDay.month,
+                'day': prevDay.day
+            },
+        'nextDay': {
+                'year': nextDay.year,
+                'month': nextDay.month,
+                'day': nextDay.day
+            },
         'today': today
     }
 
     return render(request, 'calendars/daily.html', context)
 
-def weekly(request):
+def weekly(request, yearArg=-1, monthArg=-1, dayArg=-1):
     cal = calendar.Calendar(0)
-    today = datetime.date.today()
+    if yearArg == -1 and monthArg == -1 and dayArg == -1:
+        today = datetime.date.today()
+    else:
+        today = datetime.date(yearArg, monthArg, dayArg)
+
+    delta = datetime.timedelta(weeks=1)
+    prevWeek = today - delta
+    nextWeek = today + delta
 
     month = cal.monthdatescalendar(today.year, today.month)
     for week in month:
         if today in week:
             currentWeek = week
+            firstDay = week[0]
+            lastDay = week[6]
     
     context = {
+        'prevWeek': {
+                'year': prevWeek.year,
+                'month': prevWeek.month,
+                'day': prevWeek.day
+            },
+        'currentWeek': {
+                'firstDay': firstDay,
+                'lastDay': lastDay
+            },
+        'nextWeek': {
+                'year': nextWeek.year,
+                'month': nextWeek.month,
+                'day': nextWeek.day
+            },
         'week': currentWeek
     }
 
