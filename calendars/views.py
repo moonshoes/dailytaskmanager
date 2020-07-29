0da32728020bb86143c3fcd55e5583dad395c10e
Elise <elisemgielen@gmail.com>
2020-07-29 11:25:05 +0200

    Task updates, although update button still needs to be double clicked. Not fixing that now, made it a new task!

--- calendars/views.py
+++ calendars/views.py
@@ -180,6 +180,15 @@ class UnfinishedTasksListView(ListView):
 class TaskDetailView(BSModalReadView):
     model = Task
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        next = self.request.GET.get('next', '/')
+
+        context['next'] = next
+        return context
+
+
 class FutureEventsListView(ListView):
     model = Event
     template_name = 'calendars/future_events.html'
