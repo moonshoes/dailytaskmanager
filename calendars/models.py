8587eba7e9610d1db9481c514b347dfc85549005
Elise <elisemgielen@gmail.com>
2020-08-02 11:30:11 +0200

    List of habits done

--- calendars/models.py
+++ calendars/models.py
@@ -41,3 +41,31 @@ class Habit(CalendarEntry):
 
     def __str__(self):
         return self.name
+    
+    def frequency(self):
+        frequency = ""
+
+        if (self.monday and self.tuesday and self.wednesday and self.thursday 
+            and self.friday and self.saturday and self.sunday):
+            frequency = "Every day"
+        else:
+            if self.monday:
+                frequency = frequency + "Mon, "
+            if self.tuesday:
+                frequency = frequency + "Tues, "
+            if self.wednesday:
+                frequency = frequency + "Wed, "
+            if self.thursday:
+                frequency = frequency + "Thurs, "
+            if self.friday:
+                frequency = frequency + "Fri, "
+            if self.saturday:
+                frequency = frequency + "Sat, "
+            if self.sunday:
+                frequency = frequency + "Sun"
+            
+            lengthF = len(frequency)
+            if lengthF != 0 and frequency[lengthF-1] == " ":
+                frequency = frequency[:lengthF-2]
+            
+        return frequency
